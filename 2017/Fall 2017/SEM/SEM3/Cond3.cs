a9c358fde4002971f828115bc587ccbd215c60cf
Andermaid <31659731+Andermaid@users.noreply.github.com>
2017-09-29 19:37:59 +0300

    Add files via upload

---
+++ 2017/Fall 2017/SEM/SEM3/Cond3.cs
@@ -0,0 +1,28 @@
+﻿/*Cond3. (1493. В одном шаге от счастья) Дан номер трамвайного билета, 
+ * в котором суммы первых трёх цифр и последних трёх цифр отличаются на 1 (но не сказано, в какую сторону). 
+ * Правда ли, что предыдущий или следующий билет счастливый?*/
+using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            long number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Предыдущий номер счастливый - " + CheckLuck(number - 1));
+            Console.WriteLine("Следующий номер счастливый - " + CheckLuck(number + 1));
+            Console.ReadLine();
+        }
+        static bool CheckLuck(long number)
+        {
+            int firstHalf = (int)(number / 1000);
+            int secondHalf = (int)(number - (firstHalf * 1000));
+            return SumOfNumbers(firstHalf) == SumOfNumbers(secondHalf);
+        }
+        static int SumOfNumbers(int number)
+        {
+            return (number / 100) + (number % 10) + ((number / 10) - ((number / 100) * 10));
+        }
+    }
+}
