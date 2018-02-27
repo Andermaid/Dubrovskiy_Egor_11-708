0ac041674aef676e6dec2aa146cf23b97aa0e35c
Andermaid <31659731+Andermaid@users.noreply.github.com>
2018-02-27 15:16:49 +0300

    Add files via upload

---
+++ 2017/Spring 2018/MISC/Grafs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] points = new int[n];
+            while (true)
+            {
+                int first = int.Parse(Console.ReadLine());
+                int second = int.Parse(Console.ReadLine());
+                if (points[first] > 0)
+                    points[second] = first;
+                else if (points[second] > 0)
+                    points[first] = second;
+                else
+                {
+                    points[first] = first;
+                    points[second] = first;
+                }
+
+            }
+        }
+    }
+}
