9168162ce3c0c8f535d8c5eec7c0b91013244576
Andermaid <31659731+Andermaid@users.noreply.github.com>
2018-04-12 13:26:05 +0300

    Add files via upload

---
+++ 2017/Spring 2018/MISC/KontrLinq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program<T>
+    {
+        static IEnumerable<string> MakeNewSequenceLinq(IEnumerable<string> a, IEnumerable<string> b)
+        {
+            var result =
+                from first in a
+                from second in b
+                select (string)(first + "." + second.Where(n => first.Count(char.IsLetter) == second.Count(char.IsLetter) ||
+                                                           (first.Count(char.IsDigit) == second.Count(char.IsDigit) &&
+                                                            first.Count(char.IsLetter) < second.Count(char.IsLetter))));
+            return result
+                .OrderByDescending(n => n);
+        }
+    }
+}
