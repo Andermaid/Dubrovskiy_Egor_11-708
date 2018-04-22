8c7159ffb0819d74c93d48dc4bf8e8619c63abf8
Andermaid <31659731+Andermaid@users.noreply.github.com>
2018-04-22 22:03:32 +0300

    Add files via upload

---
+++ 2017/Spring 2018/AISD/Sem2/Sem2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace Sem2
+{
+    class Program
+    {
+        static void PigeonholeSort(int[] array)
+        {
+            //Вычисляем диапозон возможных значений.
+            int max = array.Max();
+            int min = array.Min();
+            int range = max - min + 1;
+            //Создаём массив temp и для каждого элемента значения i в temp[i - min] записываем количество его вхождений.
+            int[] temp = new int[range];
+            int count = array.Length;
+            for (int i = 0; i < count; i++)
+                temp[array[i] - min]++;
+            //Переписываем изначальный массив.
+            int b = 0;
+            for (int i = 0; i < range; i++)
+                for (int j = 0; j < temp[i]; j++)
+                {
+                    array[b] = i;
+                    b++;
+                }
+        }
+
+        static void Quicksort(int[] array)
+        {
+            if (array.Length > 1) Quicksort(array, 0, array.Length - 1);
+        }
+
+        static void Quicksort(int[] array, int left, int right)
+        {
+            if (left == right) return;
+            int i = left + 1;
+            int j = right;
+            int pivot = array[left];
+            while (i < j)
+            {
+                if (array[i] <= pivot) i++;
+                else if (array[j] > pivot) j--;
+                else
+                {
+                    int m = array[i]; array[i] = array[j]; array[j] = m;
+                }
+            }
+            if (array[j] <= pivot)
+            {
+                int m = array[left]; array[left] = array[right]; array[right] = m;
+                Quicksort(array, left, right - 1);
+            }
+            else
+            {
+                Quicksort(array, left, i - 1);
+                Quicksort(array, i, right);
+            }
+        }
+    }
+}
