5d1211d3cb52f37f71ee2a83da5bdf52fa407d61
Andermaid <31659731+Andermaid@users.noreply.github.com>
2018-05-17 21:02:24 +0300

    Add files via upload

---
+++ 2017/Spring 2018/AISD/Sem3/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrieTree
+{
+    class TreeNode<T>
+    {
+        public Dictionary<char, TreeNode<T>> Childs { get; set; }
+        public T Value { get; set; }
+        public bool HasValue { get; set; }
+
+        public TreeNode(bool hasValue)
+        {
+            Childs = new Dictionary<char, TreeNode<T>>();
+            HasValue = hasValue;
+        }
+    }
+}
---
+++ 2017/Spring 2018/AISD/Sem3/TrieTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TrieTree
+{
+    class TrieTree<T>
+    {
+        TreeNode<T> Root = new TreeNode<T>(false);
+
+        public void Add(string key, T value)
+        {
+            TreeNode<T> current = Root;
+            foreach (char c in key)
+            {
+                if (current.Childs[c] == null) current.Childs[c] = new TreeNode<T>(false);
+                current = current.Childs[c];
+            }
+            current.Value = value;
+            current.HasValue = true;
+        }
+
+        public T GetValue(string key)
+        {
+            TreeNode<T> current = Root;
+            foreach (var c in key)
+            {
+                if (current.Childs[c] == null)
+                    throw new ArgumentException();
+                current = current.Childs[c];
+            }
+            if (current.HasValue)
+                return current.Value;
+            else
+                throw new ArgumentException();
+        }
+
+        public void Remove(string key)
+        {
+            TreeNode<T> current = Root;
+            foreach (var c in key)
+            {
+                if (current.Childs[c] == null)
+                    throw new ArgumentException();
+                current = current.Childs[c];
+            }
+            current.HasValue = false;
+            current.Value = default(T);
+        }
+    }
+}
